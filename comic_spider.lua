b7255e6012ca0f51a93393cf05cc89209c2e591b
y.s <y.s.inside@gmail.com>
2012-04-26 19:10:25 +0800

    Version 1.0.0.1

--- comic_spider.lua
+++ comic_spider.lua
@@ -5,7 +5,7 @@ April 2012 y.s.
 
 app_info =
 {
-	version = '1.0.0.0',
+	version = '1.0.0.1',
 	notice = 'A newer version of Comic Spider has been found, click here to download.',
-	url = ''
+	url = 'https://github.com/downloads/ysmood/ComicSpider/Comic_Spider.zip'
 }
