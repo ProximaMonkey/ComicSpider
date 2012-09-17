16d6a4600f6ebb649a8bc634862dee9d3fa0c52b
y.s <y.s.inside@gmail.com>
2012-09-18 02:28:17 +0800

    Fix a bug in batch indexing.

--- ComicSpider/Class/Spider.cs
+++ ComicSpider/Class/Spider.cs
@@ -126,7 +126,7 @@ namespace ys
 
 							foreach (var vol_dir in volume_dirs)
 							{
-								sw.WriteLine("volume_list.push('.." + vol_dir.Replace(root_dir, "").Replace('\\', '/').Replace("'", "\\'") + "/index.html');");
+								sw.WriteLine("volume_list.push('.." + vol_dir.Replace(comic_dir, "").Replace('\\', '/').Replace("'", "\\'") + "/index.html');");
 								Create_display_page(vol_dir);
 							}
 							sw.Close();
